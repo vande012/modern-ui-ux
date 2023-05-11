8398be4d84dff1ef6ea4526af490b796a4e56c35
Ryan <vandehey12@gmail.com>
2023-05-11 17:01:05 -0500

    first commit

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './possibility.css';
+import possibilityImage from '../../assets/possibility.png';
+
+const Possibility = () => {
+  return (
+    <div className='gpt3__possibility section__padding'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt='possibility' />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores cupiditate sint minus, voluptate officiis similique accusamus qui ut repellendus. Sint veniam nobis beatae unde voluptates, numquam atque aperiam? Eum, mollitia.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+
+  )
+}
+
+export default Possibility
