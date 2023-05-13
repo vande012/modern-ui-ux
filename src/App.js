79863370da1840098623d83cee9f848055db95b0
Ryan <vandehey12@gmail.com>
2023-05-13 10:50:31 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './contain
 import { CTA, Brand, Navbar } from './components';
 
 
+
 const App = () => {
   return (
     <div className='App'>
       <div className='gradient__bg'>
         <Navbar />
+        
+
         <Header />
       </div>
       <Brand />
